1b33f3c5d9d9c56184b1c2e5050108ccca6d7e50
山河映心 <17703862603@189.cn>
2023-05-21 16:40:08 +0800

    .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80, // 宽度
+  tabWidth: 2, // 缩进
+  useTabs: false, // 空格代替tab缩进
+  semi: true, // 句末使用分号
+  singleQuote: true, // 使用单引号
+  quoteProps: 'as-needed', // 仅在必需时为对象的key添加引号
+  trailingComma: 'all', // 多行时尽可能打印尾随逗号
+  bracketSpacing: true, // 对象{ }支架间距
+  arrowParens: 'always', // 箭头函数参数使用圆括号 eg: (x) => x
+  proseWrap: 'preserve', // 超出换行
+  embeddedLanguageFormatting: 'auto', // 对引用代码进行格式化
+
+  htmlWhitespaceSensitivity: 'ignore', // 对HTML全局空白不敏感
+};
